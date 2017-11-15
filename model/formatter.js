02bfc0999ad501ef61ad9c4c18e7196bd2effc7b
ffanizza10xcoding <ffanizza@10xcoding.com>
2017-11-15 09:52:36 -0600

    Commit before adding comments

--- model/formatter.js
+++ model/formatter.js
@@ -1,12 +1,6 @@
 sap.ui.define([], function () {
 	"use strict";
 	return {
-        // getOpenTicketCount : function () {
-        //     var sPath = this.getBindingContext().getPath() + '/Tickets';
-        //     var oBindings = this.getModel().bindList(sPath);
-        //     return oBindings.getLength();
-        // }
-        // ,
         getOpenTicketCount: function() {
             // var countPage = jQuery.get("https://stats0017130098trial.hanatrial.ondemand.com/dev/dev01/statapp/services/ticket.xsodata/Tickets/$count?$filter=ticketStatus_value%20ne%204");
             // var count = parseInt(countPage.responseText,10);
